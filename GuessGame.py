172391c12bf8d65b9783bc9b9cecf028b66d0f8f
Iskarr <32428946+Iskarr@users.noreply.github.com>
2019-12-10 12:22:42 -0700

    added ability to remove letter in word.

--- GuessGame.py
+++ GuessGame.py
@@ -1,31 +1,50 @@
 # This is a guesss the number game
-import random
+import random, re
 
-print("Hello, What is your name?")
-name = input()
-print("Well, " + name + ", I am thinking of a number between 1 and 20")
-secretNumber = random.randint(1, 20)
+# print("Hello, What is your name?")
+# name = input()
+# print("Well, " + name + ", Welcome to hangmanPy!")
 
-for guessesTaken in range(1, 7):
-    print("Take a guess.")
-    guess = int(input())
+secretWords = ["Tom", "Bat", "Cat", "Batman"]
 
-    if guess < secretNumber:
-        print("Your guess is too low.")
-    elif guess > secretNumber:
-        ("Your guess is too high.")
+word = random.choice(secretWords)
+
+print(word)
+
+
+def checkLetter():
+    guess = input()
+    if guess in word:
+        print("That letter is in the word.")
+        print(word.translate({ord(guess): None}))
     else:
-        break
+        print("That letter isnt in the word, try again!")
 
-if guess == secretNumber:
-    print(
-        "Good Job! "
-        + name
-        + " You guessed my number in "
-        + str(guessesTaken)
-        + " guesses."
-    )
-else:
-    print("Nope. The number I was thinking of was " + str(secretNumber))
 
-print("You took " + str(guessesTaken) + " guesses.")
+checkLetter()
+
+# secretNumber = random.randint(1, 20)
+# for guessesTaken in range(1, 7):
+#     print("Take a guess.")
+#     guess = int(input())
+
+#     if guess < secretNumber:
+#         print("Your guess is too low.")
+#     elif guess > secretNumber:
+#         ("Your guess is too high.")
+#     else:
+#         break
+
+
+# if guess == secretLetter:
+#     print(
+#         "Good Job!"
+#         + name
+#         + " You guessed my number in "
+#         + str(guessesTaken)
+#         + " guesses."
+#     )
+# else:
+#     print("Nope. The number I was thinking of was " + str(secretNumber))
+
+# print("You took " + str(guessesTaken) + " guesses.")
